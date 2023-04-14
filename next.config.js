67b436c8ca8659d194b332df93f2fc09d3876b17
r4n <ramiroreinaldo12@gmail.com>
2023-04-13 23:54:14 -0300

    Added topLevelAwait and serverComponentsExternalPacages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"]  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    return config
   },
 }
 
