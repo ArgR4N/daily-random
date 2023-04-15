6651040daf9e1dec7685243d2550974f58d8a881
r4n <ramiroreinaldo12@gmail.com>
2023-04-15 02:31:13 -0300

    Finished get of global number

---
+++ app/getGlobalNumber/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { mongoConnect } from "@/utils/mongoConnect";
+import DailyRandom from "@/utils/DailyRandom";
+
+export async function GET() {
+  await mongoConnect();
+  const globalRandom = await DailyRandom.findOne()
+    .exec()
+    .then((numbers) => {
+      if (!numbers.globalNumber) return NextResponse.json({ numbers: "No global number found" });
+      return numbers.globalNumber;
+    });
+
+  return NextResponse.json({ globalRandom });
+}
