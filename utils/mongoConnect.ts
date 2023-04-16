3c62db7b167b2307aa95c119e5c53b52f814d0af
r4n <ramiroreinaldo12@gmail.com>
2023-04-16 00:10:23 -0300

    Deleted global and cached use

--- utils/mongoConnect.ts
+++ utils/mongoConnect.ts
@@ -1,38 +1,19 @@
 import mongoose from "mongoose";
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-
-console.log(MONGODB_URI);
-
-let cached = global.mongoose;
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 export async function mongoConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
   if (!MONGODB_URI) {
     throw new Error(
       "Please define the MONGODB_URI environment variable inside .env.local"
     );
   }
 
-  if (!cached.promise) {
-    const opts = {
-      autoIndex: false,
-    };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  const opts = {
+    autoIndex: false,
+  };
+
+  mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    return mongoose;
+  });
 }
